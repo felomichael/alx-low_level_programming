0981beedc12c6ceaf2d930b7b71023db13128278
felomichael <felo.michael952004@gmail.com>
2023-02-16 13:15:19 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * this is my first c program it print by puts function not printf
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
