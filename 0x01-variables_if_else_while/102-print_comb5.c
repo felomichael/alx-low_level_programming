fbe61f5bde7792fa67f3a73be79a2f4a17a35ef5
felomichael <felo.michael952004@gmail.com>
2023-02-17 18:56:13 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 			if (num1 < num2)
 			{
 				putchar((num1 / 10) + 48);
-				putchar((num1 % 10) +48);
+				putchar((num1 % 10) + 48);
 				putchar(' ');
 				putchar((num2 / 10) + 48);
 				putchar((num2 % 10) + 48);
