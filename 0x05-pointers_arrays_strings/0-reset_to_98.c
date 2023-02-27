bb3334f6bafe679ea9734ff9955a8668f19089ab
felomichael <felo.michael952004@gmail.com>
2023-02-27 15:03:28 -0800

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * reset_to_98 - takes a pointer to int and update
+ * the value it points to 98
+ * @n: an integer input pointer
+ * Return: nothing
+ */
+void reset_to_98(int n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unisted.h>
+/**
+ * _putchar - write the character c to stdout
+ * @c: the character to print
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set approperaitely
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
