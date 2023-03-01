8c351daac63839660c134b2bcda3b065ef12892e
felomichael <felo.michael952004@gmail.com>
2023-02-28 16:00:55 -0800

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,8 +9,7 @@ int _atoi(char *s)
 	int c = 0;
 	unsigned int ni = 0;
 	int min = 1;
-	int isi = 0;
-	
+	int isi = 0;	
 	while (s[c])
 	{
 	if (s[c] ==  45)
@@ -20,7 +19,7 @@ int _atoi(char *s)
 	while (s[c] >= 48 && s[c] <= 57)
 	{
 	isi = 1;
-	ni = (ni * 10) + (s [c] - '0');
+	ni = (ni * 10) + (s[c] - '0');
 	c++;
 	}
 	if (isi == 1)
