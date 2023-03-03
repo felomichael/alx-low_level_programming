bffa9afbb9b10ccec043e29a6b7ee3de6559bb17
felomichael <felo.michael952004@gmail.com>
2023-03-03 15:40:30 -0800

    strnpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strnpy - copies a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Retrun: dest
+ */
+char *_strnpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
