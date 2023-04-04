e8120bdf6b726f7a561346fe3af8806adebe912a
Felopateer Michael <71007887+felomichael@users.noreply.github.com>
2023-04-05 01:25:27 +0200

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,42 +1,35 @@
-#include "holberton.h"
 #include <stdlib.h>
 
-/**
- * *create_array - creates an array of chars, and initializes
- *  it with a specific char.
- *  @c: char to initialize
- *  @size: number of bytes to allocate
- *
- *  Return: a pointer to the array or null if it fails
- */
-char *create_array(unsigned int size, char c)
-{
-	char *array = malloc(size);
+#include "main.h"
 
-	if (size == 0 || array == 0)
-		return (NULL);
-	while (size--)
-		array[size] = c;
-	return (array);
-}
-#include "holberton.h"
-#include <stdlib.h>
 
 /**
- * *create_array - creates an array of chars, and initializes
- *  it with a specific char.
- *  @c: char to initialize
- *  @size: number of bytes to allocate
- *
- *  Return: a pointer to the array or null if it fails
- */
-char *create_array(unsigned int size, char c)
-{
-	char *array = malloc(size);
 
-	if (size == 0 || array == 0)
-		return (NULL);
-	while (size--)
-		array[size] = c;
-	return (array);
+ * *malloc_checked - allocates memory using malloc
+
+ * @b: number of bytes to allocate
+
+ *
+
+ * Return: a pointer to the allocated memory
+
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+
+        void *ptr;
+
+
+        ptr = malloc(b);
+
+
+        if (ptr == NULL)
+
+                exit(98);
+
+
+        return (ptr);
+
 }
