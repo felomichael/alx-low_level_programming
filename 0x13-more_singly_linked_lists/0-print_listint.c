6e2cb74d2ef3811e92c450b1ae0fc6e420a75b52
Felopateer Michael <71007887+felomichael@users.noreply.github.com>
2023-04-04 17:50:32 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 
-
 /**
 
  * print_listint - prints all the elements of a linked list
@@ -19,7 +18,6 @@ size_t print_listint(const listint_t *h)
 
  size_t num = 0;
 
-
  while (h)
 
  {
@@ -31,7 +29,6 @@ size_t print_listint(const listint_t *h)
  h = h->next;
 
  }
-
 
  return (num);
 
